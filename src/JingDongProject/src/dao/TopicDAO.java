abd3947c0e79fc0bc9267df0d7338746eed82643
songminghui <1215520227@qq.com>
2018-12-19 22:59:26 +0800

    songminghui 8th commit TopicDAO.searchByTopicID ,TopicDAO.searchByTopicName,TopicDAO.searchBySomeTopicName finifshed and tested

--- src/JingDongProject/src/dao/TopicDAO.java
+++ src/JingDongProject/src/dao/TopicDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.Test;
@@ -13,7 +14,7 @@ public class TopicDAO extends DaoBase {
 	@Test
 	public void insert() {
 		Topic topic = new Topic();
-		topic.setTopicName("冬季新款");
+		topic.setTopicName("冬季女装");
 		Connection connection = getConnection();
 		PreparedStatement pStatement = null;
 		String sql = "insert topic(topicName) values(?)";
@@ -29,5 +30,83 @@ public class TopicDAO extends DaoBase {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, null);
+	}
+
+	/**
+	 * 测试 根据主题id获取主题 应该只有一条记录
+	 */
+	@Test
+	public void searchByTopicID() {
+		Topic topic = new Topic();
+		Long topicID = (long) 1;
+		Connection connection = getConnection();
+		String sql = "select * from topic where topicID=?";
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		try {
+			pStatement = connection.prepareStatement(sql);
+			pStatement.setLong(1, topicID);
+			resultSet = pStatement.executeQuery();
+			while (resultSet.next()) {
+				topic.setTopicID(resultSet.getLong("topicID"));
+				topic.setTopicName(resultSet.getString("topicName"));
+				System.out.println(topic.getTopicID() + "   " + topic.getTopicName());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		release(connection, pStatement, resultSet);
+	}
+
+	/**
+	 * 测试 根据话题名进行精确查询
+	 */
+	@Test
+	public void searchByTopicName() {
+		Topic topic = new Topic();
+		String topicName = "冬季新款";
+		Connection connection = getConnection();
+		String sql = "select * from topic where topicName=?";
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		try {
+			pStatement = connection.prepareStatement(sql);
+			pStatement.setString(1, topicName);
+			resultSet = pStatement.executeQuery();
+			while (resultSet.next()) {
+				topic.setTopicID(resultSet.getLong("topicID"));
+				topic.setTopicName(resultSet.getString("topicName"));
+				System.out.println(topic.getTopicID() + "   " + topic.getTopicName());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		release(connection, pStatement, resultSet);
+	}
+
+	/**
+	 * 测试根据话题名进行模糊查询
+	 */
+	@Test
+	public void searchBySomeTopicName() {
+		Topic topic = new Topic();
+		String topicName = "冬";
+		Connection connection = getConnection();
+		String sql = "select * from topic where topicName like ?";
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		try {
+			pStatement = connection.prepareStatement(sql);
+			pStatement.setString(1, "%" + topicName + "%");
+			resultSet = pStatement.executeQuery();
+			while (resultSet.next()) {
+				topic.setTopicID(resultSet.getLong("topicID"));
+				topic.setTopicName(resultSet.getString("topicName"));
+				System.out.println(topic.getTopicID() + "   " + topic.getTopicName());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		release(connection, pStatement, resultSet);
 	}
 }
