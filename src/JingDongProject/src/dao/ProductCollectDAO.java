4a2df8672d9a42383782ba8fe1e91accf0c9217e
soleil0510 <liangxiaoke0510@126.com>
2018-12-19 17:52:21 +0800

    liangxiaoke 2nd commit ProductCollectDAO.delete finished and tested

--- src/JingDongProject/src/dao/ProductCollectDAO.java
+++ src/JingDongProject/src/dao/ProductCollectDAO.java
@@ -2,7 +2,6 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
 import org.junit.Test;
 
 import domain.ProductCollect;
@@ -11,12 +10,13 @@ public class ProductCollectDAO extends DaoBase{
 	
 	@Test
 	public void insert(){
-		Connection connection=getConnection();
+		Connection connection=null;
 		PreparedStatement pStatement=null;
 			
 		ProductCollect productCollect=new ProductCollect();
 		productCollect.setUserID("lxk");
 		try {
+			connection=getConnection();
 			String sql="insert into productcollect(userID) values(?)";
 			pStatement=connection.prepareStatement(sql);
 			pStatement.setString(1, productCollect.getUserID());
@@ -27,6 +27,29 @@ public class ProductCollectDAO extends DaoBase{
 			else {
 				System.out.println("insert defeat!");
 			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			release(connection, pStatement, null);
+		}
+	}
+
+	@Test
+	public void delete() {
+		Connection connection=null;
+		PreparedStatement pStatement=null;
+		try {
+			connection=getConnection();
+			String sql="delete from productcollect where pcollectID=?";
+			pStatement=connection.prepareStatement(sql);
+			pStatement.setLong(1, 3);
+			int rows=pStatement.executeUpdate();
+			if(rows>0) {
+				System.out.println("delete successfully!");
+			}
+			else {
+				System.out.println("delete defeat!");
+			}	
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		} finally {
