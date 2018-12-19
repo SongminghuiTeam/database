8975c2b5e5b43c165af64f753c1df943a5bc62ef
songminghui <1215520227@qq.com>
2018-12-19 22:10:07 +0800

    songminghui 6th commit NewsDAO.searchByStoreID ,NewsDAO.searchAll,NewsDAO.deleteByNewsID finished and tested

--- src/JingDongProject/src/dao/NewsDAO.java
+++ src/JingDongProject/src/dao/NewsDAO.java
@@ -1,8 +1,9 @@
 package dao;
 
-
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,14 +12,14 @@ import org.junit.Test;
 
 import domain.News;
 
-public class NewsDAO extends DaoBase{
+public class NewsDAO extends DaoBase {
 
 	@Test
 	public void insert() {
-		News news =new News();
+		News news = new News();
 		news.setContent("衣服上新了");
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date date=new Date();
+		Date date = new Date();
 		String time = df.format(date);
 		news.setPublishTime(time);
 		news.setStoreID((long) 2);
@@ -45,4 +46,84 @@ public class NewsDAO extends DaoBase{
 		}
 		release(connection, pStatement, null);
 	}
+
+	/**
+	 * 测试 根据店铺的id进行查询
+	 */
+	@Test
+	public void searchByStoreID() {
+		News news = new News();
+		Connection connection = getConnection();
+		String sql = "select * from news where storeID=?";
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		try {
+			pStatement = connection.prepareStatement(sql);
+			pStatement.setLong(1, 2);
+			resultSet = pStatement.executeQuery();
+			while (resultSet.next()) {
+				news.setContent(resultSet.getString("content"));
+				news.setNewsID(resultSet.getLong("newsID"));
+				news.setPublishTime(resultSet.getString("publishTime"));
+				news.setStoreID(resultSet.getLong("storeID"));
+				news.setVisitVolume(resultSet.getLong("visitVolume"));
+				System.out.println(news.getNewsID() + " " + news.getStoreID() + " " + news.getContent() + " "
+						+ news.getPublishTime() + " " + news.getVisitVolume());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		release(connection, pStatement, resultSet);
+	}
+
+	/**
+	 * 测试 根据时间倒叙输出
+	 */
+	@Test
+	public void searchAll() {
+		News news = new News();
+		Connection connection = getConnection();
+		String sql = "select * from news order by newsID desc";
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		try {
+			pStatement = connection.prepareStatement(sql);
+			resultSet = pStatement.executeQuery();
+			while (resultSet.next()) {
+				news.setContent(resultSet.getString("content"));
+				news.setNewsID(resultSet.getLong("newsID"));
+				news.setPublishTime(resultSet.getString("publishTime"));
+				news.setStoreID(resultSet.getLong("storeID"));
+				news.setVisitVolume(resultSet.getLong("visitVolume"));
+				System.out.println(news.getNewsID() + " " + news.getStoreID() + " " + news.getContent() + " "
+						+ news.getPublishTime() + " " + news.getVisitVolume());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		release(connection, pStatement, resultSet);
+	}
+	
+	@Test
+	public void deleteByNewsID() {
+		
+		Long newsID =(long) 3;
+		Connection connection = getConnection();
+		String sql = "delete from news where newsID=?";
+		PreparedStatement pStatement = null;
+		
+		try {
+			pStatement = connection.prepareStatement(sql);
+			pStatement.setLong(1, newsID);
+			int resultSet = pStatement.executeUpdate();
+			if(resultSet==1)
+			{
+				System.out.println("delete news success");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		release(connection, pStatement, null);
+	}
+	
 }
