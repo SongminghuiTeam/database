f0e3813a2d80ea3a779119ce67033abe36f5598e
soleil0510 <liangxiaoke0510@126.com>
2018-12-19 23:00:44 +0800

    liangxiaoke 10th commit AddressDAO.queryByUserID finished and tested

--- src/JingDongProject/src/dao/AddressDAO.java
+++ src/JingDongProject/src/dao/AddressDAO.java
@@ -52,4 +52,54 @@ public class AddressDAO extends DaoBase{
 			release(connection, pStatement, null);
 		}
 	}
+	
+	/**
+	 * 根据userID查出该用户的所有地址信息
+	 */
+	@Test
+	public void queryByUserID() {
+		Connection connection=null;
+		PreparedStatement pStatement=null;
+		ResultSet resultset=null;	
+		
+		Address address=new Address();
+		String userID="hyl";
+		try {
+			connection=getConnection();
+			String sql="select * from address where userID=?";
+			pStatement=connection.prepareStatement(sql);
+			pStatement.setString(1, userID);
+			resultset=pStatement.executeQuery();
+			
+			System.out.println(userID+"'s all address:");
+			int i=0;
+			while(resultset.next()) {		
+				i++;
+				address.setProvince(resultset.getString("province"));
+				address.setCity(resultset.getString("city"));
+				address.setBlock(resultset.getString("block"));
+				address.setStreet(resultset.getString("street"));
+				address.setAddress(resultset.getString("address"));
+				address.setReceiver(resultset.getString("receiver"));
+				address.setPhone(resultset.getString("phone"));
+						
+				System.out.print(i+"\t");
+				System.out.print(userID+"\t");
+				
+				System.out.print(address.getProvince()+"省\t");
+				System.out.print(address.getCity()+"市\t");
+				System.out.print(address.getBlock()+"区\t");				
+				System.out.print(address.getStreet()+"街道\t");				
+				System.out.print(address.getAddress()+"\t");				
+				System.out.print(address.getReceiver()+"收\t");	
+				System.out.println(address.getPhone());
+			}				
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			release(connection, pStatement, resultset);
+		}	
+	}
+	
+	
 }
