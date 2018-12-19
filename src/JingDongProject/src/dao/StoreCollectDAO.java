56bb6f68b4b2caaf348ad670431affbd0fc650ba
soleil0510 <liangxiaoke0510@126.com>
2018-12-19 19:52:30 +0800

    liangxiaoke 4th commit StoreCollect.java finished StoreCollectDAO.insert&deleteByUserID&queryByUserID finished and tested

---
+++ src/JingDongProject/src/dao/StoreCollectDAO.java
@@ -0,0 +1,83 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.junit.Test;
+
+import domain.StoreCollect;
+
+public class StoreCollectDAO extends DaoBase{
+	
+	@Test
+	public void insert(){
+		Connection connection=null;
+		PreparedStatement pStatement=null;
+		try {
+			connection=getConnection();
+			String sql="insert into storecollect(userID) values(?)";
+			pStatement=connection.prepareStatement(sql);
+			pStatement.setString(1, "lxk");
+			int rows = pStatement.executeUpdate();
+			if(rows>0) {
+				System.out.println("insert successfully!");
+			}
+			else {
+				System.out.println("insert defeat!");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			release(connection, pStatement, null);
+		}
+	}
+
+	@Test
+	public void deleteByUserID() {
+		Connection connection=null;
+		PreparedStatement pStatement=null;
+		try {
+			connection=getConnection();
+			String sql="delete from storecollect where userID=?";
+			pStatement=connection.prepareStatement(sql);
+			pStatement.setString(1, "lxk");
+			int rows=pStatement.executeUpdate();
+			if(rows>0) {
+				System.out.println("delete successfully!");
+			}
+			else {
+				System.out.println("delete defeat!");
+			}	
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			release(connection, pStatement, null);
+		}
+	}
+	
+	@Test
+	public void queryByUserID() {
+		Connection connection=null;
+		PreparedStatement pStatement=null;
+		ResultSet resultset=null;		
+		
+		StoreCollect sCollect=new StoreCollect();
+		try {
+			connection=getConnection();
+			String sql="select * from storecollect where userID=?";
+			pStatement=connection.prepareStatement(sql);
+			pStatement.setString(1, "cwj");
+			resultset=pStatement.executeQuery();
+			while(resultset.next()) {
+				sCollect.setScollectID(resultset.getLong(1));
+				sCollect.setUserID(resultset.getString(2));
+				System.out.println(sCollect.getScollectID()+"----"+sCollect.getUserID());
+			}				
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			release(connection, pStatement, resultset);
+		}
+	}
+}
---
+++ src/JingDongProject/src/domain/StoreCollect.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class StoreCollect {
+	private Long scollectID;
+	private String userID;
+	
+	public void setScollectID(Long scollectID) {
+		this.scollectID = scollectID;
+	}
+	public Long getScollectID() {
+		return scollectID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+}
