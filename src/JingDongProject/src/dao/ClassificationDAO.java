d979ca5bac07851cc8460aea5cb3bc24214033fe
chenwanjing <735472584@qq.com>
2018-12-19 20:04:40 +0800

    chenwanjing 5th commit ClassificationDAO.updateClassificationName finished and tested ClassificationDAO.queryClassificationID finished and tested

--- src/JingDongProject/src/dao/ClassificationDAO.java
+++ src/JingDongProject/src/dao/ClassificationDAO.java
@@ -2,12 +2,13 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import domain.Classification;
 
 public class ClassificationDAO extends DaoBase{
-
+	
 	//增
 	@org.junit.Test
 	public void insert() {
@@ -46,5 +47,69 @@ public class ClassificationDAO extends DaoBase{
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	//ͨ通过classificationName查找classificationID
+	public Long queryClassificationID(String classificationName) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;	
+		ResultSet rs = null;
+		
+		Long classificationID = (long)-1;
+		
+		try {
+			conn =  this.getConnection();
+			String sql = "select Classification.classificationID from Classification where Classification.ClassificationName=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, classificationName);
+			rs = pstmt.executeQuery();
+						
+			while(rs.next()) {
+				classificationID=rs.getLong(1);
+			}
+
+			this.release(conn, pstmt, rs);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return classificationID;
+	}	
+	
+	//修改小分类名
+	@org.junit.Test
+	public void updateClassificationName() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		String oldClassificationName = "冰箱";
+		String newClassificationName = "电视机";
+		
+		ClassificationDAO cDAO = new ClassificationDAO();
+		long classificationID = cDAO.queryClassificationID(oldClassificationName);
+		
+		if(classificationID == -1) {
+			System.out.println("Update failed");
+			return;
+		}
+		
+		try {
+			conn = this.getConnection();
+			String sql = "update Classification set classificationName = ? where classificationID = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, newClassificationName);
+			pstmt.setLong(2, classificationID);
+			
+			if(pstmt.executeUpdate() > 0) {
+				System.out.println("Update successfully");
+			}
+			else {
+				System.out.println("Update failed");
+			}
+			
+			this.release(conn, pstmt, null);			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}		
 	}
 }
