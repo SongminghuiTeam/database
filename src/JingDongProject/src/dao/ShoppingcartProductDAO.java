acb470c900ab30352bcc01a6fed619ace01325c8
yolanehe <yolanehe@163.com>
2018-12-20 22:51:16 +0800

    heyulin 19th commit ShoppingcartProduct.search finished and tested

--- src/JingDongProject/src/dao/ShoppingcartProductDAO.java
+++ src/JingDongProject/src/dao/ShoppingcartProductDAO.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -47,12 +48,52 @@ public class ShoppingcartProductDAO extends DaoBase{
 		}
 	}
 	
+	public ArrayList<Long> search(Long shoppingcartID){
+		ArrayList<Long> productIDs = new ArrayList<Long>();
+		
+		Connection conn = null;
+		PreparedStatement pStatement = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			String sql = "select * from shoppingcartproduct where shoppingcartID=?";
+			pStatement = conn.prepareStatement(sql);
+			pStatement.setLong(1, shoppingcartID);
+			
+			rs = pStatement.executeQuery();
+			
+			while(rs.next()) {
+				productIDs.add(rs.getLong("productID"));
+			}
+		}catch(Exception sqlException) {
+			sqlException.printStackTrace();
+		}finally {
+			try {
+				release(conn, pStatement, rs);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return productIDs;
+	}
+	
 	@Test
 	public void Test() {
 		//ShoppingcartProduct shoppingcartProduct1 = new ShoppingcartProduct((long)2, (long)7);
 		ShoppingcartProduct shoppingcartProduct2 = new ShoppingcartProduct((long)4, (long)5);
 		
 		//insert(shoppingcartProduct1);
-		insert(shoppingcartProduct2);
+		//insert(shoppingcartProduct2);
+		
+		ArrayList<Long> productIDs = search((long)1);
+		if(productIDs.size() == 0) {
+			System.out.println("no matched record!");
+		}
+		else {
+			for(int i = 0;i < productIDs.size();i++) {
+				System.out.println("productID:" + productIDs.get(i));
+			}
+		}
 	}
 }
