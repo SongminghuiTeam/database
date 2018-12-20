334b0cbca04b422508f62766a6f37aa98f53bf6f
songminghui <1215520227@qq.com>
2018-12-20 23:16:01 +0800

    songminghui 14th commit modified newsDAO.java all method

--- src/JingDongProject/src/dao/NewsDAO.java
+++ src/JingDongProject/src/dao/NewsDAO.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.Test;
@@ -15,7 +16,7 @@ import domain.News;
 public class NewsDAO extends DaoBase {
 
 	@Test
-	public void insert() {
+	public void Test() {
 		News news = new News();
 		news.setContent("裤子上新了");
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -24,6 +25,19 @@ public class NewsDAO extends DaoBase {
 		news.setPublishTime(time);
 		news.setStoreID((long) 2);
 		news.setVisitVolume((long) 100);
+		
+		Long storeID=(long) 2;
+		String content="新";
+		Long newsID=(long) 3;
+		
+		//insert(news);
+		//searchAll();
+		//searchByStoreID(storeID);
+		//searchBySomeNewsName(content);
+		//deleteByNewsID(newsID);
+	}
+
+	public int insert(News news) {
 		Connection connection = getConnection();
 		String sql = "insert news(storeID,visitVolume,publishTime,content) values(?,?,?,?)";
 		PreparedStatement pStatement = null;
@@ -37,21 +51,23 @@ public class NewsDAO extends DaoBase {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		int rSet = 0;
 		try {
-			int rSet = pStatement.executeUpdate();
+			rSet = pStatement.executeUpdate();
 			if (rSet == 1)
 				System.out.println("insert success");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, null);
+		return rSet;
 	}
 
 	/**
 	 * 测试 根据店铺的id进行查询
 	 */
-	@Test
-	public void searchByStoreID() {
+	public ArrayList<News> searchByStoreID(Long storeID) {
+		ArrayList<News> newslist = new ArrayList<News>();
 		News news = new News();
 		Connection connection = getConnection();
 		String sql = "select * from news where storeID=?";
@@ -59,7 +75,7 @@ public class NewsDAO extends DaoBase {
 		ResultSet resultSet = null;
 		try {
 			pStatement = connection.prepareStatement(sql);
-			pStatement.setLong(1, 2);
+			pStatement.setLong(1, storeID);
 			resultSet = pStatement.executeQuery();
 			while (resultSet.next()) {
 				news.setContent(resultSet.getString("content"));
@@ -67,6 +83,7 @@ public class NewsDAO extends DaoBase {
 				news.setPublishTime(resultSet.getString("publishTime"));
 				news.setStoreID(resultSet.getLong("storeID"));
 				news.setVisitVolume(resultSet.getLong("visitVolume"));
+				newslist.add(news);
 				System.out.println(news.getNewsID() + " " + news.getStoreID() + " " + news.getContent() + " "
 						+ news.getPublishTime() + " " + news.getVisitVolume());
 			}
@@ -74,13 +91,14 @@ public class NewsDAO extends DaoBase {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, resultSet);
+		return newslist;
 	}
 
 	/**
 	 * 测试 根据时间倒叙输出
 	 */
-	@Test
-	public void searchAll() {
+	public ArrayList<News> searchAll() {
+		ArrayList<News> newslist = new ArrayList<News>();
 		News news = new News();
 		Connection connection = getConnection();
 		String sql = "select * from news order by newsID desc";
@@ -95,6 +113,7 @@ public class NewsDAO extends DaoBase {
 				news.setPublishTime(resultSet.getString("publishTime"));
 				news.setStoreID(resultSet.getLong("storeID"));
 				news.setVisitVolume(resultSet.getLong("visitVolume"));
+				newslist.add(news);
 				System.out.println(news.getNewsID() + " " + news.getStoreID() + " " + news.getContent() + " "
 						+ news.getPublishTime() + " " + news.getVisitVolume());
 			}
@@ -102,36 +121,35 @@ public class NewsDAO extends DaoBase {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, resultSet);
+		return newslist;
 	}
-	
-	@Test
-	public void deleteByNewsID() {
-		
-		Long newsID =(long) 3;
+
+	public int deleteByNewsID(Long newsID) {
+
 		Connection connection = getConnection();
 		String sql = "delete from news where newsID=?";
 		PreparedStatement pStatement = null;
-		
+		int resultSet = 0;
 		try {
 			pStatement = connection.prepareStatement(sql);
 			pStatement.setLong(1, newsID);
-			int resultSet = pStatement.executeUpdate();
-			if(resultSet==1)
-			{
+			resultSet = pStatement.executeUpdate();
+			if (resultSet == 1) {
 				System.out.println("delete news success");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, null);
+		return resultSet;
 	}
+
 	/**
 	 * 测试 根据 newsName进行模糊查询
 	 */
-	@Test
-	public void searchBySomeNewsName() {
+	public ArrayList<News> searchBySomeNewsName(String content) {
+		ArrayList<News> newslist = new ArrayList<News>();
 		News news = new News();
-		String content = "新";
 		Connection connection = getConnection();
 		String sql = "select * from news where content like ?";
 		PreparedStatement pStatement = null;
@@ -146,6 +164,7 @@ public class NewsDAO extends DaoBase {
 				news.setPublishTime(resultSet.getString("publishTime"));
 				news.setStoreID(resultSet.getLong("storeID"));
 				news.setVisitVolume(resultSet.getLong("visitVolume"));
+				newslist.add(news);
 				System.out.println(news.getNewsID() + " " + news.getStoreID() + " " + news.getContent() + " "
 						+ news.getPublishTime() + " " + news.getVisitVolume());
 			}
@@ -153,7 +172,7 @@ public class NewsDAO extends DaoBase {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, resultSet);
+		return newslist;
 	}
 
-	
 }
