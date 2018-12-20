78a587381ac17774acff0f47b5fe4e731eebbf0d
yolanehe <yolanehe@163.com>
2018-12-20 21:29:32 +0800

    heyulin 16th commit UserDAO.java modified

--- src/JingDongProject/src/dao/UserDAO.java
+++ src/JingDongProject/src/dao/UserDAO.java
@@ -13,13 +13,10 @@ import java.sql.SQLException;
 import domain.*;
 
 public class UserDAO extends DaoBase{
-	@org.junit.Test
-	public void insert() {
+	public void insert(User user) {
 		Connection conn = null;
 		PreparedStatement pStatement = null;
 		ResultSet rSet = null;
-		
-		User user = new User("songminghui","13269016600","","6543321");
 		
 		String userID = user.getUserID();
 		String mail = user.getMail();
@@ -55,263 +52,75 @@ public class UserDAO extends DaoBase{
 		}
 	}
 	
-	// login by userID
-	@org.junit.Test
-	public void searchByUserID() {
+	public void search(String userID) {
 		Connection conn = null;
 		PreparedStatement pStatement = null;
 		ResultSet rs = null;
 		
-		String username = "chenwanjing";
-		String truePassword = "222222";
-		String falsePassword = "111111";
-		
 		try {
 			conn = getConnection();
-			String sql = "select * from user where userID=" + "'" + username + "'" + " and password=" + "'" + truePassword + "'";
+			
+			String sql = "select * from user where userID=?";
 			pStatement = conn.prepareStatement(sql);
+			pStatement.setString(1, userID);
 			rs = pStatement.executeQuery();
 			
 			if(rs.next()) {
-				System.out.println("login Success");
-				System.out.println("login Info");
-				System.out.println("username:" + rs.getString("userID") + " phone:" + rs.getString("phone") + " mail:" + rs.getString("mail") + " password:" + rs.getString("password"));
+				System.out.println("userID:" + rs.getString("userID") + "   password:" + rs.getString("password"));
+				System.out.println("mail:" + rs.getString("mail") + "   phone:" + rs.getString("phone"));
+				System.out.print("\n");
 			}
 			else {
-				System.out.println("login Error");
+				System.out.println("no such user");
+				System.out.print("\n");
 			}
 		}catch(Exception sqlException) {
 			sqlException.printStackTrace();
 		}finally {
 			try {
-				release(conn, pStatement, rs);
+				release(conn, pStatement, null);
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 	
-	// login by mail
-	@org.junit.Test
-	public void searchByMail() {
+	public void update(String userID,User user) {
 		Connection conn = null;
 		PreparedStatement pStatement = null;
 		ResultSet rs = null;
 		
-		String mail = "";
-		String truePassword = "654321";
-		String falsePassword = "111111";
-		
 		try {
-			if(mail.equals("")) {
-				System.out.println("current user's mail information is empty,login error");
-			}
-			else {
-				conn = getConnection();
-				String sql = "select * from user where mail=" + "'" + mail + "'" + " and password=" + "'" + truePassword + "'";
+			conn = getConnection();
+			String sql = "select * from user where userID=?";
+			pStatement = conn.prepareStatement(sql);
+			pStatement.setString(1, userID);
+			
+			rs = pStatement.executeQuery();
+			if(rs.next()) {
+				sql = "update user set phone=?, mail=?, password=? where userID=?";
 				pStatement = conn.prepareStatement(sql);
-				rs = pStatement.executeQuery();
+				pStatement.setString(1, user.getPhone());
+				pStatement.setString(2, user.getMail());
+				pStatement.setString(3, user.getPassword());
+				pStatement.setString(4, userID);
 				
-				if(rs.next()) {
-					System.out.println("login Success");
-					System.out.println("login Info");
-					System.out.println("username:" + rs.getString("userID") + " phone:" + rs.getString("phone") + " mail:" + rs.getString("mail") + " password:" + rs.getString("password"));
+				int row = pStatement.executeUpdate();
+				
+				if(row > 0) {
+					System.out.println("update successfully!");
 				}
 				else {
-					System.out.println("login Error");
-				}
-			}
-		}catch(Exception sqlException) {
-			sqlException.printStackTrace();
-		}finally {
-			try {
-				release(conn, pStatement, rs);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	// login by phone
-	@Test
-	public void searchByPhone() {
-		Connection conn = null;
-		PreparedStatement pStatement = null;
-		ResultSet rs = null;
-		
-		String phone = "13269016600";
-		String truePassword = "654321";
-		String falsePassword = "111111";
-		
-		try {
-			conn = getConnection();
-			String sql = "select * from user where phone=" + "'" + phone + "'" + " and password=" + "'" + truePassword + "'";
-			pStatement = conn.prepareStatement(sql);
-			rs = pStatement.executeQuery();
-			
-			if(rs.next()) {
-				System.out.println("login Success");
-				System.out.println("login Info");
-				System.out.println("username:" + rs.getString("userID") + " phone:" + rs.getString("phone") + " mail:" + rs.getString("mail") + " password:" + rs.getString("password"));
-			}
-			else {
-				System.out.println("login Error");
-			}
-		}catch(Exception sqlException) {
-			sqlException.printStackTrace();
-		}finally {
-			try {
-				release(conn, pStatement, rs);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@Test
-	public void updateMail() {
-		Connection conn = null;
-		PreparedStatement pStatement = null;
-		ResultSet rs = null;
-		
-		String username = "heyulin";
-		String password = "111111";
-		String mail = "yolanehe@163.com";
-		
-		try {
-			conn = getConnection();
-			String sql = "select * from user where userID=?";
-			pStatement = conn.prepareStatement(sql);
-			pStatement.setString(1, username);
-			rs = pStatement.executeQuery();
-			
-			if(rs.next()) {
-				if(rs.getString("password").equals(password)) {
-					sql = "update user set mail=? where userID=?";
-					pStatement = conn.prepareStatement(sql);
-					pStatement.setString(1, mail);
-					pStatement.setString(2, username);
-					
-					int row = pStatement.executeUpdate();
-					if(row > 0) {
-						System.out.println("update mail successfully");
-					}
-					else {
-						System.out.println("update mail failed");
-					}	
-				}
-				else {
-					System.out.println("password Error!!!");
+					System.out.println("update failed!");
 				}
 			}
 			else {
-				System.out.println("no user named " + username);
+				System.out.println("no such user");
 			}
 		}catch(Exception sqlException) {
 			sqlException.printStackTrace();
 		}finally {
 			try {
-				release(conn, pStatement, rs);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@Test
-	public void updatePhone() {
-		Connection conn = null;
-		PreparedStatement pStatement = null;
-		ResultSet rs = null;
-		
-		String username = "heyulin";
-		String password = "111111";
-		String phone = "15113158871";
-		
-		try {
-			conn = getConnection();
-			String sql = "select * from user where userID=?";
-			pStatement = conn.prepareStatement(sql);
-			pStatement.setString(1, username);
-			rs = pStatement.executeQuery();
-			
-			if(rs.next()) {
-				if(rs.getString("password").equals(password)) {
-					sql = "update user set phone=? where userID=?";
-					pStatement = conn.prepareStatement(sql);
-					pStatement.setString(1, phone);
-					pStatement.setString(2, username);
-					
-					int row = pStatement.executeUpdate();
-					if(row > 0) {
-						System.out.println("update phone successfully");
-					}
-					else {
-						System.out.println("update phone failed");
-					}
-				}
-				else {
-					System.out.println("password Error!!!");
-				}
-			}
-			else {
-				System.out.println("no user named " + username);
-			}
-		}catch(Exception sqlException) {
-			sqlException.printStackTrace();
-		}finally {
-			try {
-				release(conn, pStatement, rs);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@Test
-	public void updatePassword() {
-		Connection conn = null;
-		PreparedStatement pStatement = null;
-		ResultSet rs = null;
-		
-		String username = "chenwanjing";
-		String newPassword = "222222";
-		String oldPassword = "asdfghj";
-		
-		try {
-			conn = getConnection();
-			String sql = "select * from user where userID=?";
-			pStatement = conn.prepareStatement(sql);
-			pStatement.setString(1, username);
-			rs = pStatement.executeQuery();
-			
-			if(rs.next()) {
-				if(rs.getString("password").equals(oldPassword)) {
-					sql = "update user set password=? where userID=?";
-					pStatement = conn.prepareStatement(sql);
-					pStatement.setString(1, newPassword);
-					pStatement.setString(2, username);
-					
-					int row = pStatement.executeUpdate();
-					if(row > 0) {
-						System.out.println("update password successfully");
-					}
-					else {
-						System.out.println("update password failed");
-					}
-				}
-				else {
-					System.out.println("password Error!!!");
-				}
-			}
-			else {
-				System.out.println("no user named " + username);
-			}
-		}catch(Exception sqlException) {
-			sqlException.printStackTrace();
-		}finally {
-			try {
-				System.out.println("testtesttest");
 				release(conn, pStatement, rs);
 			}catch(Exception e) {
 				e.printStackTrace();
@@ -320,19 +129,16 @@ public class UserDAO extends DaoBase{
 	}
 	
 	// delete column in detailed with foreign-key at the same time
-	@Test
-	public void delete() {
+	public void delete(String userID) {
 		Connection conn = null;
 		PreparedStatement pStatement = null;
 		ResultSet rs = null;
-		
-		String username = "liangxiaoke";
 		
 		try {
 			conn = getConnection();
 			String sql = "delete from user where userID=?";
 			pStatement = conn.prepareStatement(sql);
-			pStatement.setString(1, username);
+			pStatement.setString(1, userID);
 			
 			int row = pStatement.executeUpdate();
 			if(row > 0) {
@@ -352,29 +158,20 @@ public class UserDAO extends DaoBase{
 		}
 	}
 	
-	public boolean isExist(String userID) {
-		Connection conn = null;
-		PreparedStatement pStatement = null;
-		ResultSet rs = null;
-		try {
-			conn = getConnection();
-			String sql = "select * from user where userID=?";
-			pStatement = conn.prepareStatement(sql);
-			pStatement.setString(1, userID);
-			rs = pStatement.executeQuery();
-			
-			if(rs.next())
-				return true;
-			
-		}catch(Exception sqlException) {
-			sqlException.printStackTrace();
-		}finally {
-			try {
-				release(conn, pStatement, rs);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
+	@Test
+	public void Test() {
+		User user = new User("heyulin", "13602339963", "yolanehe@163.com", "yolane0401");
+		//insert(user);
+		search("heyulin");
+		search("cwj");
+		search("katherine");
+		
+		User newUser = new User("heyulin", "15113158871", "294813616@qq.com", "123456");
+		// update("heyulin", newUser);
+		search("heyulin");
+		search("cwj");
+		search("katherine");
+		
+		delete("heyulin");
 	}
 }
