c787affc3cbd2a801fad60f93a449a92afa8cd39
songminghui <1215520227@qq.com>
2018-12-20 23:50:17 +0800

    songminghui 15th commit modified TopicnewsDAO.java all method

--- src/JingDongProject/src/dao/TopicNewsDAO.java
+++ src/JingDongProject/src/dao/TopicNewsDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -17,33 +18,41 @@ public class TopicNewsDAO extends DaoBase {
 	 * topicid与newsid又是外键，所以在topic表和news表中不存在的数值是不对的
 	 */
 	@Test
-	public void insert() {
+	public void Test() {
 		Topic topic = new Topic();
 		News news = new News();
 		topic.setTopicID((long) 2);
 		news.setNewsID((long) 2);
+
+		//insert(topic, news);
+		//searchNewsIDByTopicID(topic);
+		searchTopicIDBYNewsID(news);
+	}
+
+	public int insert(Topic topic, News news) {
 		Connection connection = getConnection();
 		PreparedStatement pStatement = null;
 		String sql = "insert topicnews(topicID,newsID) values(?,?)";
+		int resultSet = 0;
 		try {
 			pStatement = connection.prepareStatement(sql);
 			pStatement.setLong(1, topic.getTopicID());
 			pStatement.setLong(2, news.getNewsID());
-			int resultSet = pStatement.executeUpdate();
+			resultSet = pStatement.executeUpdate();
 			if (resultSet == 1)
 				System.out.println("insert topicnews success");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, null);
+		return resultSet;
 	}
 
 	/**
 	 * 根据topicid查找newsid ————在搜寻topic时使用
 	 */
-	@Test
-	public void searchNewsIDByTopicID() {
-		Topic topic = new Topic();
+	public ArrayList<Long> searchNewsIDByTopicID(Topic topic) {
+		ArrayList<Long> newsids = new ArrayList<Long>();
 		News news = new News();
 		topic.setTopicID((long) 1);
 		Connection connection = getConnection();
@@ -52,9 +61,11 @@ public class TopicNewsDAO extends DaoBase {
 		String sql = "select newsid from topicnews where topicid=?";
 		try {
 			pStatement = connection.prepareStatement(sql);
+			pStatement.setLong(1, topic.getTopicID());
 			resultSet = pStatement.executeQuery();
 			while (resultSet.next()) {
 				news.setNewsID(resultSet.getLong("newsid"));
+				newsids.add(news.getNewsID());
 				System.out.println(news.getNewsID());
 			}
 		} catch (SQLException e) {
@@ -62,16 +73,15 @@ public class TopicNewsDAO extends DaoBase {
 			e.printStackTrace();
 		}
 		release(connection, pStatement, resultSet);
+		return newsids;
 	}
 
 	/**
 	 * 根据newsid 查找topicid
 	 */
-	@Test
-	public void searchTopicIDBYNewsID() {
+	public ArrayList<Long> searchTopicIDBYNewsID(News news) {
+		ArrayList<Long> topicids = new ArrayList<Long>();
 		Topic topic = new Topic();
-		News news = new News();
-		news.setNewsID((long) 2);
 		Connection connection = getConnection();
 		PreparedStatement pStatement = null;
 		ResultSet resultSet = null;
@@ -82,13 +92,14 @@ public class TopicNewsDAO extends DaoBase {
 			resultSet = pStatement.executeQuery();
 			while (resultSet.next()) {
 				topic.setTopicID(resultSet.getLong("topicid"));
+				topicids.add(topic.getTopicID());
 				System.out.println(topic.getTopicID());
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		release(connection, pStatement, resultSet);
+		return topicids;
 	}
 
 }
